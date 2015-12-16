27011433883c4b01bbb6a1968fb05fe1c222816f
akma327 <akma327@stanford.edu>
2015-12-16 14:43:28 -0800

    Created xcode project

--- Jesus Take the Wheel/Jesus Take the Wheel/ViewController.h
+++ Jesus Take the Wheel/Jesus Take the Wheel/ViewController.h
@@ -7,9 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet FBSDKLoginButton *loginButton;
 
 @end
 
